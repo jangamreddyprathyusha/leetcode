c8171aa4298fb08448decfd9e2333addfc5e40fd
jangamreddyprathyusha <jangamreddyprathu@gmail.com>
2026-08-17 16:57:56 +0530

    Time: 2 ms (30.99%), Space: 12.4 MB (69.61%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        prefix=""
+        for i in range(len(strs[0])):
+            ch=strs[0][i]
+            for s in strs:
+                if i>=len(s) or s[i]!=ch:
+                  return prefix  
+            prefix+=ch
+        return prefix
+
+
+        
